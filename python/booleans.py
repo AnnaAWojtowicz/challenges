1e6b8fc25410a7b408e97f060bbd72c026f17990
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-11-16 17:12:31 +0100

    bool

---
+++ python/booleans.py
@@ -0,0 +1,12 @@
+# PRINT.TYPE((STR))
+
+#If you want to know the type of a specific value, you can use the built-in function type:
+
+# text data
+text = 'Hello World'
+print(type(text))       #<class 'str'>
+ 
+# whole number
+num = 123
+print(type(num))        #<class 'int'>
+
