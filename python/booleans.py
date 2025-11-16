0738b78348cc7fb5992a40d82ac91700a8e2ffda
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-11-16 17:38:59 +0100

    bools

--- python/booleans.py
+++ python/booleans.py
@@ -38,3 +38,62 @@ print('Paris' == 'Paris')           # True
 
 # is the sum of 2 and 5 in range?
 print(0 < 2 + 5 < 10)           # True
+
+
+# AND
+# In Python, we can use the Logical Operator and to combine the 2 conditions:
+
+is_favorite_color = True
+is_within_budget = False
+ 
+print(is_favorite_color and is_within_budget)       # False
+
+# The result is False because and requires both conditions to be true.
+
+
+
+# OR
+
+# Another Logical Operator in Python is or. 
+# or only requires one of the two conditions to be true.
+
+# Let's what happens if we replace and with or:
+
+is_favorite_color = True
+is_within_budget = False
+ 
+print(is_favorite_color or is_within_budget)        # True
+
+
+
+# NOT
+
+# The final Logical Operator in Python is not, which reverses the boolean value of an expression.
+
+# For example, let's assume we have a traffic light. If the traffic light is not green, we must stop:
+
+isGreen = False
+ 
+mustStop = not isGreen
+ 
+print(mustStop)         # True
+# Here, isGreen is set to False, and we use not to reverse its value.
+
+
+# You can even combine multiple Logical Operators. For example:
+
+print(True and not False)       # True
+# Here, we get True because not reverts False and both conditions are True.
+
+
+#In Python, logical operators are evaluated from left to right. 
+# The order of evaluation is determined by operator precedence, 
+# with not having the highest precedence, followed by and, and then or.
+
+# However, you can use parentheses to specify the order of evaluation explicitly, 
+# making the code easier to read. For example:
+
+print((True or False) and (True and False))     # False
+
+
+
