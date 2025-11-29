18e8e98e4e0063c8b8063cb4caee367bb1bb52dd
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2025-11-29 11:58:25 +0100

    functions2

--- python/functions2.py
+++ python/functions2.py
@@ -22,6 +22,57 @@ def my_function():
  
 my_function()
 print(text)     # Hello World
+
+# Even after calling the function, the variable text still holds its original value.
+# It seems like nothing changed, even though we assigned a new value to text inside the function.
+# To understand what happened here, you need to know that we are dealing with two so-called scopes.
+# A scope describes a region of code from which you can access certain variables or functions.
+# When we first created the variable text, we were creating it in the global scope.
+##### global scope #####
+text = 'Hello World' # global variable
+print(text)     # Hello world
+
+# The global scope is the outermost scope in your program. Whenever you create a variable outside a function (or a class), you are defining a global variable within the global scope. 
+# However, when you create a function, the code inside the function has its own scope, known as local scope.
+
+#### global scope ####
+...
+ 
+def my_function():
+  #### local scope ####
+  ...
+
+
+# You can access all global variables inside the local scope of a function. For example:
+
+#### global scope ####
+my_num = 10
+ 
+def my_function():
+  #### local scope ####
+  print(my_num) # local variable    # 10
+ 
+my_function()
+
+
+# However, when we create a variable inside the function, we're defining a so-called local variable that can only be accessed in the local scope of that function.
+# For example, here we create a local variable my_num inside the local scope of my_function. When we try to access that variable outside the function, we run into an error:
+def my_function():
+  #### local scope ####
+  my_num = 10 # local variable
+ 
+#### global scope ####
+my_function()
+ 
+#print(my_num)   # NameError: name 'my_num' is not defined
+
+
+
+# 
+# 
+# 
+# 
+# 
 # 
 # 
 # 
